27d0a3deaeefbef20634f87b20a74ce7fc187c2b
Liangfu Chen <liangfu.chen@nlpr.ia.ac.cn>
2015-09-09 11:32:34 +0800

    convert csv table to json format for building treeview in web;

---
+++ content/csv2json.py
@@ -0,0 +1,45 @@
+
+import csv
+import json
+from pprint import pprint
+
+
+csvfile = open('bnatlas_246.csv', 'r')
+jsonfile = open('bnatlas_246.json', 'w')
+
+fieldnames = ("index","id","lobe","gyrus","area")
+reader = csv.DictReader( csvfile, fieldnames)
+
+lobes=[]
+gyruses=[]
+result=[]
+
+# for row in reader:
+#   json.dump(row, jsonfile)
+#   jsonfile.write(',\n')
+
+for row in reader:
+  index=row['index']
+  ID = row['id']
+  lobe = row['lobe'].capitalize()
+  gyrus = row['gyrus'].capitalize()
+  if lobe not in lobes:
+    lobes.append(lobe)
+    result.append({'text':lobe})
+    lidx=lobes.index(lobe)
+    result[lidx]['children']=[]
+  lidx=lobes.index(lobe)
+  gyruses = [item['text'] for item in result[lidx]['children']]
+  if gyrus not in gyruses:
+    gyruses.append(gyrus)
+    result[lidx]['children'].append({'text':gyrus})
+    gidx=gyruses.index(gyrus)
+    result[lidx]['children'][gidx]['children']=[]
+  gidx=gyruses.index(gyrus)
+  result[lidx]['children'][gidx]['children'].append({'text':ID,'id':index})
+  
+  
+# print lobe,gyrus
+# pprint(result)
+print json.dumps(result, indent=2, separators=(',',':'))
+json.dump(result, jsonfile, indent=0, separators=(',',':'))

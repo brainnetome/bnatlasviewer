a7015f2e00459f6b1dfdca60cfaf2fba904437fb
Liangfu Chen <liangfu.chen@nlpr.ia.ac.cn>
2015-09-09 16:12:59 +0800

    generate both behaviorial domain and paradigm class data for displaying on the web;

---
+++ content/bdpc.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import os,sys,json
+
+# custom parameters
+datadir = 'BDPC_20150908'
+typelist=['BDf_FDR05','PCf_FDR05']
+
+for typeidx in range(2):
+  datatype=typelist[typeidx]
+  filename_fmt = datadir+'/Cluster%d_'+datatype+'.txt'
+  outfile = datatype+'.json'
+
+  # initialize
+  idx = 1
+  ncluster = 246
+
+  result = {}
+
+  for idx in range(1,ncluster+1):
+    filename = filename_fmt % idx
+    fp = open(filename)
+    if not fp: break
+    print 'processing %s ...' % filename
+
+    thisdict = {}
+    while 1:
+      linestr = fp.readline()
+      if not linestr: break;
+      line = linestr.split()
+      label = '_'.join(line[0:len(line)-1])#line[0]
+      value = float(line[-1])
+      thisdict[label] = value
+      # print {line[0]:float(line[1])}
+    print thisdict
+
+    result[idx]=thisdict
+  #   break
+  # break
+
+  fp = open(outfile,'wt')
+  fp.write(json.dumps(result, indent=0, sort_keys=True))
